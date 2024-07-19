e37cf1090fdae1abc2d86790a7e3ede014ead240
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-07-19 20:31:11 +0000

    .

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -24,8 +24,7 @@ export default function Footer() {
           <Link href="/">{siteMetadata.title}</Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/DeFi-1">``DFI1``
-          </Link>
+          <Link href="https://github.com/DeFi-1">DFI1</Link>
         </div>
       </div>
     </footer>
