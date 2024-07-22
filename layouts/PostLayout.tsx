66e7b46fbc78840f4f67af17cd7d0ece723ede36
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-07-22 14:07:13 -0400

    Update PostLayout.tsx

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -102,7 +102,7 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
               </div>
               {siteMetadata.comments && (
                 <div
-                  className="pb-6 pt-6 text-center text-gray-700 dark:text-gray-300"
+                  className="pb-6 pt-6 text-center text-gray-700 dark:text-gray-300 giscus"
                   id="comment"
                 >
                   <Comments slug={slug} />
