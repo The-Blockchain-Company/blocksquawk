85fe704c26704c51e3326b9370c230ea2a1105c0
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-07-22 12:04:20 -0400

    Update ScrollTopAndComment.tsx

--- components/ScrollTopAndComment.tsx
+++ components/ScrollTopAndComment.tsx
@@ -26,7 +26,6 @@ const ScrollTopAndComment = () => {
     <div
       className={`fixed bottom-8 right-8 hidden flex-col gap-3 ${show ? 'md:flex' : 'md:hidden'}`}
     >
-
       {siteMetadata.comments?.provider && (
         <button
           aria-label="Scroll To Comment"
