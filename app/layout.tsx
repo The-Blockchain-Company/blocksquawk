2b1cf30751081e3dae30f3da4f0872de9c6b9715
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-07-22 18:06:42 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -82,21 +82,23 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 <Header />
                 <main className="mb-auto">{children}</main>
               </SearchProvider>
-<Script src="https://giscus.app/client.js"
-        data-repo="The-Blockchain-Company/blocksquawk"
-        data-repo-id="R_kgDOLjDLrA"
-        data-category="Blog Comments"
-        data-category-id="DIC_kwDOLjDLrM4ChE_g"
-        data-mapping="pathname"
-        data-strict="0"
-        data-reactions-enabled="1"
-        data-emit-metadata="1"
-        data-input-position="top"
-        data-theme="preferred_color_scheme"
-        data-lang="en"
-        data-loading="lazy"
-        crossOrigin="anonymous"
-        async
+
+`<Script 
+    src="https://giscus.app/client.js"
+    data-repo="The-Blockchain-Company/           blocksquawk"
+    data-repo-id="R_kgDOLjDLrA"
+    data-category="Blog Comments"
+    data-category-id="DIC_kwDOLjDLrM4ChE_g"
+    data-mapping="pathname"
+    data-strict="0"
+    data-reactions-enabled="1"
+    data-emit-metadata="1"
+    data-input-position="top"
+    data-theme="preferred_color_scheme"
+    data-lang="en"
+    data-loading="lazy"
+    crossOrigin="anonymous"
+    async
         />
               <Footer />
             </div>
