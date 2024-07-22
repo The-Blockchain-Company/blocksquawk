f8d5b6eac517df39e300bc6807a18bf0a4231c22
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-07-22 21:25:35 +0000

    colon

--- next.config.js
+++ next.config.js
@@ -6,11 +6,11 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
-  default-src 'self';
+  default-src 'self' 'unsafe-eval';
   script-src 'self' 'unsafe-eval' 'unsafe-inline' https://giscus.app https://apis.google.com analytics.umami.is;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
-  child-src https://giscus.app
+  child-src https://giscus.app;
   media-src *;
   connect-src *;
   font-src 'self';
