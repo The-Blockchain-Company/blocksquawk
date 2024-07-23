76640ea0600c67e35438ce3a523d109fdf037b0e
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-07-23 08:26:27 -0400

    Update Comments.tsx

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -9,8 +9,8 @@ export default function Comments({ slug }: { slug: string }) {
   return (
     <>
       {!loadComments && <button onClick={() => setLoadComments(true)}>Load Comments</button>}
-      {siteMetadata.comments && loadComments && (
-        <CommentsComponent commentsConfig={siteMetadata.comments} slug={slug} />
+      {siteMetadata.comments.provider && loadComments && (
+        <CommentsComponent commentsConfig={siteMetadata.comments.provider} slug={slug} />
       )}
     </>
   )
